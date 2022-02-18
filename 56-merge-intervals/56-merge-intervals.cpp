8c3a9d2563f13b02f0ca8f0ff30550c374428426
Thomas_Shelby <76743956+shoaibshazid@users.noreply.github.com>
2022-02-18 08:13:14 +0530

    Time: 260 ms (5.03%), Space: 18.9 MB (58.78%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>mergeIntervals;
-        if(intervals.size()==0){
-            return mergeIntervals;
-        }
+        int n=intervals.size();
+        vector<vector<int>>ans;
+        if(n==0) return ans;
         sort(intervals.begin(),intervals.end());
-        vector<int>tempInterval=intervals[0];
-        for(auto &it:intervals){
-            if(it[0]<=tempInterval[1]){
-                tempInterval[1]=max(it[1],tempInterval[1]);
-            }else{
-                mergeIntervals.push_back(tempInterval);
-                tempInterval=it;
+        for(int i=0;i<n;i++){
+            int start=intervals[i][0];
+            int end=intervals[i][1];
+            if(!ans.empty()){
+                if(start<=ans.back()[1]) continue;
             }
+            for(int j=i+1;j<n;j++){
+               if (intervals[j][0] <= end) { 
+                   end=max(end,intervals[j][1]);
+                 }
+            }
+            ans.push_back({start,end});
         }
-        mergeIntervals.push_back(tempInterval);
-        return mergeIntervals;
+        return ans;
     }
 };
